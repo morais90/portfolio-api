d24e9be276c313923fd5b66e02a5cb16df496465
Willian de Morais <williandmorais@gmail.com>
2016-10-06 20:14:17 -0300

    Configuração para DecimalField

--- bellacia/essay/models.py
+++ bellacia/essay/models.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 class Section(models.Model):
     hooker = models.ForeignKey('hooker.Hooker')
     public = models.BooleanField(default=False)
-    cost = models.DecimalField(default=0.99)
+    cost = models.DecimalField(max_digits=5, decimal_places=2, default=0.99)
 
 
 class Photo(models.Model):

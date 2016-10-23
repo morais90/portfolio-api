dbc798b7587c2596453defd9a352ab078bf77304
Willian de Morais <williandmorais@gmail.com>
2016-10-23 13:54:31 -0200

    Arrumado imports

--- bellacia/authentication/api.py
+++ bellacia/authentication/api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
 from .models import User
-from .serializer import UserSerializer
+from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ViewSet):

35a56ea7215c5ed077144abfe4e7e5b8e917de2c
Willian de Morais <williandmorais@gmail.com>
2016-10-30 20:03:19 -0200

    Adicionado rta para GroupViewSet e PermissionViewSet

--- bellacia/users/urls.py
+++ bellacia/users/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from rest_framework import routers
-from .api import UserViewSet
+from .api import UserViewSet, GroupViewSet, PermissionViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'permissions', PermissionViewSet)
 
 urlpatterns = router.urls

50b8f044adc3a3e97117e053700cd6b63003d934
Willian de Morais <williandmorais@gmail.com>
2016-10-30 11:16:08 -0200

    Modificado rota para users que estava errada

--- bellacia/users/urls.py
+++ bellacia/users/urls.py
@@ -3,5 +3,6 @@ from rest_framework import routers
 from .api import UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'', UserViewSet)
+
 urlpatterns = router.urls

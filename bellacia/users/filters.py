821fa1b7c31d49be840ead3b49685cf413b0f7f5
Willian de Morais <williandmorais@gmail.com>
2016-10-30 20:00:19 -0200

    Adicionado GroupFilter e PermissionFilter

--- bellacia/users/filters.py
+++ bellacia/users/filters.py
@@ -3,7 +3,7 @@ import django_filters
 from django.db.models import Q
 from rest_framework.filters import FilterSet
 
-from .models import User
+from .models import User, Group, Permission
 
 
 class UserFilter(FilterSet):
@@ -18,3 +18,20 @@ class UserFilter(FilterSet):
     class Meta:
         model = User
         fields = ('id', 'name', 'email', 'is_active')
+
+
+class GroupFilter(FilterSet):
+    name = django_filters.CharFilter(name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = Group
+        fields = ('id', 'name')
+
+
+class PermissionFilter(FilterSet):
+    name = django_filters.CharFilter(name='name', lookup_expr='icontains')
+    codename = django_filters.CharFilter(name='codename', lookup_expr='icontains')
+
+    class Meta:
+        model = Permission
+        fields = ('id', 'name', 'codename')

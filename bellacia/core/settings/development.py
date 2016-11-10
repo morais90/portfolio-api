ea9f12bca3f2425d6a26c092cb89919c506ba26b
Willian de Morais <williandmorais@gmail.com>
2016-11-10 21:21:42 -0200

    Adicionado cors middleware

--- bellacia/core/settings/development.py
+++ bellacia/core/settings/development.py
@@ -1,26 +1,12 @@
-from .production import *
+from .production import *  # noqa
 
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa
     }
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+MIDDLEWARE_CLASSES.append('cargobr_api.extra.middlewares.CorsMiddleware');  # noqa

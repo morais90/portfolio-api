547048c6ab8c1dc59b226af04d4e86fdbd6d8d5e
Willian de Morais <williandmorais@gmail.com>
2016-11-16 23:00:31 -0200

    Adicionado tratamento para OPTIONS

--- bellacia/core/middlewares.py
+++ bellacia/core/middlewares.py
@@ -5,7 +5,11 @@ from django import http
 def cors_middleware(get_response):
     def middleware(request):
         if request.method == 'OPTIONS':
-            return http.HttpResponse()
+            response = http.HttpResponse()
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization'
+            response['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+            return response
 
         response = get_response(request)
         response['Access-Control-Allow-Origin'] = '*'

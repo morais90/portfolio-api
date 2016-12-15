7b4a04e7b9b20cc70b130ef281df6d601333da49
Willian de Morais <williandmorais@gmail.com>
2016-12-15 08:11:32 -0200

    Arrumado urls para hooker

--- bellacia/core/urls.py
+++ bellacia/core/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^auth/', include('bellacia.authentications.urls')),
-    url(r'^users/', include('bellacia.users.urls'))
+    url(r'^users/', include('bellacia.users.urls')),
+    url(r'^hookers/', include('bellacia.hooker.urls'))
 ]

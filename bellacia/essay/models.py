dd57e1f9f5f2ccc1a0369e8996bbeafc393ab692
Willian Morais <williandmorais@gmail.com>
2017-04-14 18:29:26 -0300

    Arrumado campos

--- bellacia/essay/models.py
+++ bellacia/essay/models.py
@@ -3,18 +3,19 @@ from django.db import models
 
 
 class Section(models.Model):
-    hooker = models.ForeignKey('hooker.Hooker')
-    public = models.BooleanField(default=False)
+    model = models.ForeignKey('model.Model')
     cost = models.DecimalField(max_digits=5, decimal_places=2, default=0.99)
 
 
 class Photo(models.Model):
     section = models.ForeignKey(Section)
     image = models.ImageField(upload_to='section-photos')
+    public = models.BooleanField(default=False)
     description = models.TextField()
 
 
 class Video(models.Model):
     section = models.ForeignKey(Section)
     video = models.FileField(upload_to='section-videos')
+    public = models.BooleanField(default=False)
     description = models.TextField()

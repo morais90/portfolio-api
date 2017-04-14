26b114568606a4e6917dfc18090306eca8872bc7
Willian Morais <williandmorais@gmail.com>
2017-04-14 18:29:51 -0300

    Arrumado urls com novos apps

--- bellacia/core/urls.py
+++ bellacia/core/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^auth/', include('bellacia.authentications.urls')),
-    url(r'^users/', include('bellacia.users.urls')),
-    url(r'^hookers/', include('bellacia.hooker.urls'))
+    url(r'^auth/', include('bellacia.authentication.urls')),
+    url(r'^users/', include('bellacia.user.urls')),
+    url(r'^hookers/', include('bellacia.model.urls'))
 ]

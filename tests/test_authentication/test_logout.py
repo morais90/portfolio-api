0d81090bff8aabfe633ae4bd8a29087544674058
Willian Morais <williandmorais@gmail.com>
2017-04-21 20:51:57 -0300

    Teste para deslogar

---
+++ tests/test_authentication/test_logout.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import patch
+from bellacia.core.tests import ExtendedTestCase
+from bellacia.user.models import User
+
+
+class TestLogout(ExtendedTestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='williandmorais@gmail.com', password='mudar123')
+
+    def test_invalid_auth(self):
+        response = self.client.delete(
+            '/v1/auth/',
+            HTTP_AUTHORIZATION=self.bearer_auth('williandmorais@gmail.com', 'test123')
+        )
+
+        self.assertDictEqual(response.json(), {'error': 'invalid_credential'})
+        self.assertEqual(response.status_code, 401)
+
+    @patch.multiple('uuid.UUID', hex='9db5c6bffdbb4be4a77bd3be68c7d9eb')
+    @patch('django.core.cache.cache.delete')
+    def test_logout(self, cache_mock):
+        response = self.client.delete(
+            '/v1/auth/',
+            HTTP_AUTHORIZATION=self.bearer_auth('williandmorais@gmail.com', 'mudar123')
+        )
+
+        cache_mock.assert_called_with(b'9db5c6bffdbb4be4a77bd3be68c7d9eb')
+        self.assertEqual(response.status_code, 204)
